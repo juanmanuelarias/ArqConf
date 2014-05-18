62587c533a7f18490d797c61e0acbb69097c536a
Juan Arias <juanmanuelarias@gmail.com>
2014-05-18 18:33:03 -0300

    add heroku deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -308,6 +308,18 @@ module.exports = function (grunt) {
                         'styles/fonts/{,*/}*.*',
                         'bower_components/' + (this.includeCompass ? 'sass-' : '') + 'bootstrap/' + (this.includeCompass ? 'fonts/' : 'dist/fonts/') +'*.*'
                     ]
+                }, {
+                expand: true,
+                    dest: '<%= yeoman.dist %>',
+                    cwd: 'heroku',
+                    src: '*',
+                    rename: function (dest, src) {
+                        var path = require('path');
+                        if (src === 'distpackage.json') {
+                            return path.join(dest, 'package.json');
+                        }
+                        return path.join(dest, src);
+                    }
                 }]
             },
             styles: {
@@ -395,7 +407,7 @@ module.exports = function (grunt) {
         'cssmin',
         'uglify',
         'copy:dist',
-        'modernizr',
+        //'modernizr',
         'rev',
         'usemin',
         'htmlmin'
