72a048cf39efbf6d7bf378f4dc759e3638a4fbae
Juan Arias <juanmanuelarias@gmail.com>
2014-05-18 16:25:19 -0300

    refactor, move js to main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+var offset = 50;
+  $('nav a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - offset
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
