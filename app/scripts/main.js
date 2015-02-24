c9114b38e28fc48bd400f338ecf3ec9248c469df
Nelson Ghezzi <nelsonghezzi@gmail.com>
2015-02-23 22:50:27 -0300

    Added countdown to landing section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,7 @@ $(function() {
   animateNav();
 
   new WOW().init();
+  setupCountdown('2015/04/30', ['dias', 'horas', 'minutos', 'segundos'], $('#countdown-template'), $('#countdown-container'));
 });
 
 function configureScroll () {
@@ -105,3 +106,69 @@ function animateNav () {
     js.src = "http://bettercontactform.com/contact/media/d/6/d6ae80b1b686bc01ef3f667f33e40fa5c4bb1ae1.js";
     fjs.parentNode.insertBefore(js, fjs);
     }(document, "script", "bcf-render"));
+
+function setupCountdown(targetDate, labels, templateElement, targetElement) {
+    var template = _.template(templateElement.html()),
+        currDate = '00:00:00:00:00',
+        nextDate = '00:00:00:00:00',
+        parser = /([0-9]{2})/gi;
+
+    // Parse countdown string to an object
+    function strfobj(str) {
+        var parsed = str.match(parser),
+            obj = {};
+        labels.forEach(function (label, i) {
+            obj[label] = parsed[i]
+        });
+        return obj;
+    }
+
+    // Return the time components that diffs
+    function diff(obj1, obj2) {
+        var diff = [];
+        labels.forEach(function (key) {
+            if (obj1[key] !== obj2[key]) {
+                diff.push(key);
+            }
+        });
+        return diff;
+    }
+
+    // Build the layout
+    var initData = strfobj(currDate);
+    labels.forEach(function (label, i) {
+        targetElement.append(template({
+            curr: initData[label],
+            next: initData[label],
+            label: label
+        }));
+    });
+
+    // Starts the countdown
+    targetElement.countdown(targetDate, function (event) {
+        var newDate = event.strftime('%D:%H:%M:%S'),
+            data;
+        if (newDate !== nextDate) {
+            currDate = nextDate;
+            nextDate = newDate;
+            // Setup the data
+            data = {
+                'curr': strfobj(currDate),
+                'next': strfobj(nextDate)
+            };
+            // Apply the new values to each node that changed
+            diff(data.curr, data.next).forEach(function (label) {
+                var selector = '.%s'.replace(/%s/, label),
+                    $node = targetElement.find(selector);
+                // Update the node
+                $node.removeClass('flip');
+                $node.find('.curr').text(data.curr[label]);
+                $node.find('.next').text(data.next[label]);
+                // Wait for a repaint to then flip
+                _.delay(function ($node) {
+                    $node.addClass('flip');
+                }, 50, $node);
+            });
+        }
+    });
+}
