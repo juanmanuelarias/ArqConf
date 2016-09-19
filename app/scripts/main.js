dfa495d7d50cf4749efe1bc85a9d2c0396135e9e
asdsa <asd>
2016-09-18 23:39:42 -0300

    Some changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,7 @@ $(function() {
   animateNav();
 
   new WOW().init();
-  setupCountdown('2015/04/30 18:00:00', ['dias', 'horas', 'minutos', 'segundos'], $('#countdown-template'), $('#countdown-container'));
+  setupCountdown('2016/08/04 18:00:00', ['dias', 'horas', 'minutos', 'segundos'], $('#countdown-template'), $('#countdown-container'));
 });
 
 function configureScroll () {
